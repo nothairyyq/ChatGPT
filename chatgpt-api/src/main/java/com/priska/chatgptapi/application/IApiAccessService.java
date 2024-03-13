ab21053645ef3b95088e18e6c2e1b051298b5537
nothairyyq <yaqiyu@uw.edu>
2024-03-13 15:58:36 -0700

    update JWTUtils

--- chatgpt-api/src/main/java/com/priska/chatgptapi/application/IApiAccessService.java
+++ chatgpt-api/src/main/java/com/priska/chatgptapi/application/IApiAccessService.java
@@ -1,4 +1,4 @@
 package com.priska.chatgptapi.application;
-
+//以后会增加相关业务逻辑方法
 public interface IApiAccessService {
 }
--- chatgpt-api/src/main/java/com/priska/chatgptapi/domain/security/model/vo/JwtToken.java
+++ chatgpt-api/src/main/java/com/priska/chatgptapi/domain/security/model/vo/JwtToken.java
@@ -1,4 +1,24 @@
 package com.priska.chatgptapi.domain.security.model.vo;
 
-public class JwtToken {
+import org.apache.shiro.authc.AuthenticationToken;
+
+
+//Token 的对象信息，你可以设置用户ID、用户密码
+public class JwtToken implements AuthenticationToken{
+    private String jwt;
+    public JwtToken(String jwt){
+        this.jwt = jwt;
+    }
+
+    @Override
+    public Object getPrincipal(){
+        return jwt;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return jwt;
+    }
+
+
 }
