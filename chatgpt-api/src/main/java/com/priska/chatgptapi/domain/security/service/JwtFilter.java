e17ee5c54c86decac7543ece972f07117f707787
nothairyyq <yaqiyu@uw.edu>
2024-03-13 17:49:18 -0700

    finish shiro&JWT

--- chatgpt-api/src/main/java/com/priska/chatgptapi/domain/security/service/JwtFilter.java
+++ chatgpt-api/src/main/java/com/priska/chatgptapi/domain/security/service/JwtFilter.java
@@ -1,4 +1,50 @@
 package com.priska.chatgptapi.domain.security.service;
 
-public class JwtFilter {
+import com.priska.chatgptapi.domain.security.model.vo.JwtToken;
+import org.apache.shiro.web.filter.AccessControlFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/*
+* 自定义Filter, 用于拦截携带Token的请求
+* */
+public class JwtFilter extends AccessControlFilter{
+
+    private Logger logger = LoggerFactory.getLogger(JwtFilter.class);
+
+    @Override
+    protected boolean isAccessAllowed(ServletRequest servletRequest, ServletResponse servletResponse, Object o) throws Exception {
+        return false;
+    }
+
+    @Override
+    protected boolean onAccessDenied(ServletRequest servletRequest, ServletResponse servletResponse) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        // 如果你设定的 token 放到 header 中，则可以这样获取；request.getHeader("Authorization");
+        JwtToken jwtToken = new JwtToken(request.getParameter("token"));
+        try {
+            // 鉴权认证
+            getSubject(servletRequest, servletResponse).login(jwtToken);
+            return true;
+        } catch (Exception e) {
+            logger.error("鉴权认证失败", e);
+            onLoginFail(servletResponse);
+            return false;
+        }
+    }
+
+    /**
+     * 鉴权认证失败时默认返回 401 状态码
+     */
+    private void onLoginFail(ServletResponse response) throws IOException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        httpResponse.getWriter().write("Auth Err!");
+    }
 }
